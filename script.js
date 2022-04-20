b72a9ed08d61a8a867889895faa59cd71d5bbfc6
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-20 13:48:12 +0200

    finish

--- script.js
+++ script.js
@@ -4,24 +4,52 @@
 
 const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+//numero da 1 a 10
+// function RandomWordNumber(){
+//     return Math.ceil(Math.random() * 10);
+// }
+// console.log(RandomWordNumber());
 
-function RandomWordNumber(){
-    return Math.ceil(Math.random() * 10);
-}
 
-RandomWordNumber();
-let frase = "";
-function WordGenerator(totAbc){
+
+//con numero da 1 a dieci creare una parola
+function WordGenerator(){
+    let parola = "";
+    let totAbc = Math.ceil(Math.random() * 10);
     for(i = 1; i <= totAbc; i++){
-        x = Math.ceil(Math.random()*abc.length );
-        console.log(x, "x")
-        frase += abc[x];
-        if(i===totAbc){
-            frase+= " ";
-        }
+        x = Math.ceil(Math.random()*abc.length -1 );
+        // console.log(x, "x")
+    //    console.log(abc[x]);
+       parola+= abc[x];
     }
+    return parola
+}
+console.log(WordGenerator())
+
+//mi serve un geratore di frasi che genera tot parole
+function generatoreFrasi(numeroDiParole){
+    
+    let frase = "";
+    for(let i = 0; i < numeroDiParole; i++){
+        let fraseups = WordGenerator()+ " ";
+        console.log(i, "frase ",fraseups);
+
+        frase += fraseups
+    }
+    return frase
 }
 
-WordGenerator(RandomWordNumber())
+console.log(generatoreFrasi(10))
 
-console.log(frase)
+
+
+// function TotWordsGenerator(index){
+//     const askwordnumber = index
+//     for(i=0; i < askwordnumber; i++ ){
+//         totWords = totWords + WordGenerator() + " " ;
+//     }
+//     console.log(totWords, "aaa2")
+//     return totWords
+// }
+
+// console.log(TotWordsGenerator(4))
